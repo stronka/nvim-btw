5f1b2746d4c9f7b076dad5ba5644ab5f224dc531
chrsec <stronka.k@gmail.com>
2025-02-13 22:54:47 +0100

    compile & multicursor

--- init.lua
+++ init.lua
@@ -93,6 +93,9 @@ Plug('lukas-reineke/indent-blankline.nvim')
 -- TODO: might not need this one
 Plug('ryanoasis/vim-devicons')
 
+-- Mutliple cursor - TODO examine
+Plug('jake-stewart/multicursor.nvim')
+
 vim.call('plug#end')
 
 -- Plugin config
@@ -181,6 +184,123 @@ vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
 vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
 
+local mc = require('multicursor-nvim')
+
+local mc_setup = function()
+    mc.setup()
+
+    local set = vim.keymap.set
+
+    -- Add or skip cursor above/below the main cursor.
+    set({"n", "x"}, "<up>",
+        function() mc.lineAddCursor(-1) end)
+    set({"n", "x"}, "<down>",
+        function() mc.lineAddCursor(1) end)
+    set({"n", "x"}, "<leader><up>",
+        function() mc.lineSkipCursor(-1) end)
+    set({"n", "x"}, "<leader><down>",
+        function() mc.lineSkipCursor(1) end)
+
+    -- Add or skip adding a new cursor by matching word/selection
+    set({"n", "x"}, "<M-n>",
+        function() mc.matchAddCursor(0) end)
+    set({"n", "x"}, "<M-s>",
+        function() mc.matchSkipCursor(1) end)
+    set({"n", "x"}, "<M-N>",
+        function() mc.matchAddCursor(-1) end)
+    set({"n", "x"}, "<M-S>",
+        function() mc.matchSkipCursor(-1) end)
+
+    -- In normal/visual mode, press `mwap` will create a cursor in every match of
+    -- the word captured by `iw` (or visually selected range) inside the bigger
+    -- range specified by `ap`. Useful to replace a word inside a function, e.g. mwif.
+    set({"n", "x"}, "mw", function()
+        mc.operator({ motion = "iw", visual = true })
+        -- Or you can pass a pattern, press `mwi{` will select every \w,
+        -- basically every char in a `{ a, b, c, d }`.
+        -- mc.operator({ pattern = [[\<\w]] })
+    end)
+
+    -- Press `mWi"ap` will create a cursor in every match of string captured by `i"` inside range `ap`.
+    set("n", "mW", mc.operator)
+
+    -- Add all matches in the document
+    set({"n", "x"}, "<leader>A", mc.matchAllAddCursors)
+
+    -- You can also add cursors with any motion you prefer:
+    -- set("n", "<right>", function()
+    --     mc.addCursor("w")
+    -- end)
+    -- set("n", "<leader><right>", function()
+    --     mc.skipCursor("w")
+    -- end)
+
+    -- Rotate the main cursor.
+    set({"n", "x"}, "<left>", mc.nextCursor)
+    set({"n", "x"}, "<right>", mc.prevCursor)
+
+    -- Delete the main cursor.
+    set({"n", "x"}, "<M-k>", mc.deleteCursor)
+
+    -- Add and remove cursors with control + left click.
+    set("n", "<c-leftmouse>", mc.handleMouse)
+    set("n", "<c-leftdrag>", mc.handleMouseDrag)
+
+    -- Easy way to add and remove cursors using the main cursor.
+    set({"n", "x"}, "<c-q>", mc.toggleCursor)
+
+    -- Clone every cursor and disable the originals.
+    set({"n", "x"}, "<leader><c-q>", mc.duplicateCursors)
+
+    set("n", "<esc>", function()
+        if not mc.cursorsEnabled() then
+            mc.enableCursors()
+        elseif mc.hasCursors() then
+            mc.clearCursors()
+        else
+            -- Default <esc> handler.
+        end
+    end)
+
+    -- bring back cursors if you accidentally clear them
+    set("n", "<leader>gv", mc.restoreCursors)
+
+    -- Align cursor columns.
+    set("n", "<leader>a", mc.alignCursors)
+
+    -- Split visual selections by regex.
+    set("x", "S", mc.splitCursors)
+
+    -- Append/insert for each line of visual selections.
+    set("x", "I", mc.insertVisual)
+    set("x", "A", mc.appendVisual)
+
+    -- match new cursors within visual selections by regex.
+    set("x", "M", mc.matchCursors)
+
+    -- Rotate visual selection contents.
+    set("x", "<leader>t",
+        function() mc.transposeCursors(1) end)
+    set("x", "<leader>T",
+        function() mc.transposeCursors(-1) end)
+
+    -- Jumplist support
+    set({"x", "n"}, "<c-i>", mc.jumpForward)
+    set({"x", "n"}, "<c-o>", mc.jumpBackward)
+
+    -- Customize how cursors look.
+    local hl = vim.api.nvim_set_hl
+    hl(0, "MultiCursorCursor", { link = "Cursor" })
+    hl(0, "MultiCursorVisual", { link = "Visual" })
+    hl(0, "MultiCursorSign", { link = "SignColumn"})
+    hl(0, "MultiCursorMatchPreview", { link = "Search" })
+    hl(0, "MultiCursorDisabledCursor", { link = "Visual" })
+    hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+    hl(0, "MultiCursorDisabledSign", { link = "SignColumn"})
+end
+
+mc_setup()
+
 
 -- Mine
 
@@ -208,3 +328,9 @@ vim.cmd([[
     nnoremap <leader>cl VyV:!bc -l<esc>0PJa=<space><esc>
 ]])
 
+-- Extra config hook - run at the end
+local nvim_extra_config = os.getenv("NVIM_EXTRA_CONFIG")
+
+if nvim_extra_config then
+    dofile(nvim_extra_config)
+end
--- lua/makefile.lua
+++ lua/makefile.lua
@@ -1,22 +1,106 @@
 local M = {}
 local vim = vim
 
+local compile_cmd = 'make'
+local result_window = -1
+local buf = -1
+
+local split_string = function(str, pattern)
+    if rawequal(str, nil) then
+        return {}
+    end
+
+    local tokens = {}
+
+    for line in str:gmatch(pattern) do
+        table.insert(tokens, line)
+    end
+
+    if rawequal(next(tokens), nil) then
+        return {str}
+    end
+
+    return tokens
+end
+
+local run_compilation = function()
+    local current_window = vim.api.nvim_get_current_win()
+
+    if vim.api.nvim_win_is_valid(result_window) then
+        vim.api.nvim_win_close(result_window, true)
+    end
+
+    if vim.api.nvim_buf_is_valid(buf) then
+        vim.api.nvim_buf_delete(buf, { force = true })
+    end
+
+
+    result_window = vim.api.nvim_open_win(0, false, {
+        split = 'right',
+        win = 0
+    })
+
+    buf = vim.api.nvim_create_buf(false, true)
+
+    vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
+    vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+    vim.api.nvim_buf_set_option(buf, 'number', false)
+    vim.api.nvim_buf_set_option(buf, 'relativenumber', false)
+    vim.api.nvim_win_set_buf(result_window, buf)
+
+    vim.api.nvim_set_current_win(current_window)
+
+    local update_buffer = function(data)
+        vim.api.nvim_buf_set_lines(buf, -1, -1, false, split_string(data, "[^\r\n]+"))
+    end
+
+    vim.system(split_string(compile_cmd, "[^ ]+"), {
+        text = true,
+        stdout = vim.schedule_wrap(function(_, data)
+            update_buffer(data)
+        end),
+        stderr = vim.schedule_wrap(function(_, data)
+            update_buffer(data)
+        end),
+        on_exit = vim.schedule_wrap(function()
+            vim.api.nvim_buf_set_option(buf, 'readonly', true)
+        end)
+    })
+end
+
+local compile = function()
+    vim.ui.input({ prompt = 'Compile: ' }, function(input)
+        compile_cmd = input
+        run_compilation()
+    end)
+end
+
+
 M.setup = function()
     vim.keymap.set(
         'n',
-        '<F8>',
-        function()
-            vim.cmd('make')
-        end
+        '<F7>',
+        compile
     )
 
     vim.keymap.set(
         'n',
-        '<F9>',
+        '<C-F7>',
+        run_compilation
+    )
+
+    vim.keymap.set(
+        'n',
+        '<M-F7>',
         function()
-            vim.cmd('make run')
+            if vim.api.nvim_win_is_valid(result_window) then
+                vim.api.nvim_win_close(result_window, true)
+                result_window = -1
+            end
         end
     )
 end
 
 return M
+
+
